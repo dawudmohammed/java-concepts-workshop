708ad6f6d29f33905faa03bd03bd964b0186a326
dawudmohammed <dawud.mohammed@turntabl.io>
2021-11-03 14:51:52 +0000

    update

--- overloading/src/Addition.java
+++ overloading/src/Addition.java
@@ -10,12 +10,10 @@ public class Addition {
        return x+y;
     }
 
-    public int add(int x ,int y, int z){
-        return x+y+z;
-    }
-
     public float add(int x ,int y, float z){
         return x+y+z;
     }
+
+
 }
 
